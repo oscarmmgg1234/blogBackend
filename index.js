8a2c787ceb55508595cfc355edb192d9ea5740dd
Oscar Maldonado <oscarmmgg1234@gmail.com>
2026-01-13 19:08:20 -0500

    fixed trust proxy express use

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ server.use(
   })
 );
 
-app.set("trust proxy", true);
+server.set("trust proxy", true);
 server.use(express.json({ limit: "50mb" }));
 server.use(express.urlencoded({ extended: true, limit: "50mb" }));
 server.use(router);
